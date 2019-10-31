4ee00fc5533c8ff6a83c0b5e9a40f9a62927bd17
brunopeople <brunopessoa.melo@gmail.com>
2019-10-30 21:32:07 -0300

    subindo o projeto para o repositório, criando o primeiro componente

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script>
+  export deafult{
+    name: 'app',
+    componentes:{},
+    data(){
+     return{
+     videos:[],
+     reformattedSearchString: '';
+     api{
+     baseUrl: 'https://www.googleapis.com/youtube/v3/search?',
+     part: 'snippet',
+     type: 'video',
+     order: 'viewCount',
+     maxResults: 12,
+     q: '',
+     key: 'AIzaSyDiM2IecFkkxiQABc81-AIHmXDi1o2Ei8o'
+     prePageToken: '',
+     nextPageToken: ''
+      }
+     };
+   },
+
+   methods:{}
+    };
+</script>
